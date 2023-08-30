ede6c12e7b5fabe6714be4deed2faeff47a8048a
Shoaibnadeemsk <102457785+Shoaibnadeemsk@users.noreply.github.com>
2023-08-30 16:30:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example_instance" {
+resource "aws_instance" "example_instances" {
   ami           = "ami-08a52ddb321b32a8c"  # Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "ExampleInstances"
   }
 }
